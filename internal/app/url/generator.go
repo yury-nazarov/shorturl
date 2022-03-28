18daf815d0ac6e76628e91418a3e5f645d4309d5
Yuri Nazarov <jnazarov@gpmdata.ru>
2022-03-28 09:52:24 +0300

    fix typo

--- internal/app/url/generator.go
+++ internal/app/url/generator.go
@@ -10,10 +10,10 @@ import (
 
 // GenerateShortLink алгоритм сокращения URL на основе base58 - для получения
 //					 набора символов которые человеком могут читатся однозначно.
-func GenerateShortLink(initialLink string, shortUrlLength int) string {
+func GenerateShortLink(initialLink string, URLLength int) string {
 	generatedNumber := new(big.Int).SetBytes([]byte(initialLink)).Uint64()
 	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
-	return finalString[:shortUrlLength]
+	return finalString[:URLLength]
 }
 
 func base58Encoded(bytes []byte) string {
