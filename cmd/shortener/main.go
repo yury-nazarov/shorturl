b76038554967f1148743e22251997153cf0975f7
Yuri Nazarov <jnazarov@gpmdata.ru>
2022-04-12 21:43:11 +0300

    increment_8: debug

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 
 	"github.com/yury-nazarov/shorturl/internal/app/handler"
-	appMiddleware "github.com/yury-nazarov/shorturl/internal/app/middleware"
 	"github.com/yury-nazarov/shorturl/internal/app/service"
 	"github.com/yury-nazarov/shorturl/internal/app/storage"
 	"github.com/yury-nazarov/shorturl/internal/app/storage/filedb"
@@ -55,8 +54,9 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	// Собственные middleware
-	r.Use(appMiddleware.HTTPResponseCompressor)
+	//r.Use(appMiddleware.HTTPResponseCompressor)
 	//r.Use(appMiddleware.HTTPRequestDecompressor)
+	r.Use(middleware.Compress(5, "gzip"))
 
 	// Создаем объект для доступа к методам компрессии URL
 	lc := service.NewLinkCompressor(5,  baseURL)
