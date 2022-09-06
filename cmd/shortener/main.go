8bc82615a41a3887f8835c7790df55ce1636ddd2
Yury <yury.s.nazarov@gmail.com>
2022-09-06 23:37:27 +0300

    fix  [REFACTORING]: clean

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,41 +1,34 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/yury-nazarov/shorturl/internal/app/handler"
 	"github.com/yury-nazarov/shorturl/internal/app/service"
 	"github.com/yury-nazarov/shorturl/internal/app/storage"
 	"github.com/yury-nazarov/shorturl/internal/config"
 	"github.com/yury-nazarov/shorturl/internal/logger"
+	"log"
+	"net/http"
 )
 
-
 func main() {
-	run()
-}
-
-func run() {
 	// ИНициируем логгер
-	log := logger.New()
+	logger := logger.New()
 
 	// Инициируем конфиг: аргументы cli > env
 	cfg, err := config.NewConfig()
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
-
 	// Инициируем БД
 	db := storage.New(storage.DBConfig{FileName: cfg.FileStoragePath, PGConnStr: cfg.DatabaseDSN})
-
 	// Создаем объект для доступа к методам компрессии URL
 	lc := service.NewLinkCompressor(5, cfg.BaseURL)
 	// Инициируем объект для доступа к хендлерам
 	c := handler.NewController(db, lc)
-
+	// Инициируем роутер
 	r := handler.NewRouter(c, db)
-
 	// Запускаем сервер
 	log.Println("run server on", cfg.ServerAddress)
 	log.Fatal(http.ListenAndServe(cfg.ServerAddress, r))
 }
+
